6020bd53b7e89e8a15772521aaf37ab32c0df5c5
saix511 <saadirfan41@gmail.com>
2021-12-13 19:08:55 +0500

    fix

--- component/FormSteps/Step2.tsx
+++ component/FormSteps/Step2.tsx
@@ -16,7 +16,7 @@ import * as yup from 'yup'
 // const visaRegEx = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/
 const Step2 = ({ setvalues, setcurrentstep, prevvalues }: any) => {
   return (
-    <div className='mt-8 w-1/3'>
+    <div className='mt-8 w-screen'>
       <Formik
         initialValues={{
           nameOnCard: prevvalues.nameOnCard,
@@ -60,92 +60,94 @@ const Step2 = ({ setvalues, setcurrentstep, prevvalues }: any) => {
 
           isSubmitting,
         }) => (
-          <Card className=''>
-            <CardBody>
-              <h1 className='flex justify-center font-semibold text-lg'>
-                Payment Details
-              </h1>
-              <form onSubmit={handleSubmit}>
-                <div className='pb-5 pt-8'>
-                  <InputIcon
-                    name='nameOnCard'
-                    defaultValue={prevvalues.nameOnCard}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    type='text'
-                    color='indigo'
-                    size='regular'
-                    outline={true}
-                    placeholder='Name on Card'
-                    iconFamily='material-icons'
-                    iconName='account_circle'
-                    error={
-                      errors.nameOnCard &&
-                      touched.nameOnCard &&
-                      errors.nameOnCard
-                    }
-                    success={
-                      !errors.nameOnCard && touched.nameOnCard ? 'Valid' : ''
-                    }
-                  />
-                </div>
+          <div className='max-w-3xl mx-auto '>
+            <Card>
+              <CardBody>
+                <h1 className='flex justify-center font-semibold text-lg'>
+                  Payment Details
+                </h1>
+                <form onSubmit={handleSubmit}>
+                  <div className='pb-5 pt-8'>
+                    <InputIcon
+                      name='nameOnCard'
+                      defaultValue={prevvalues.nameOnCard}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      type='text'
+                      color='indigo'
+                      size='regular'
+                      outline={true}
+                      placeholder='Name on Card'
+                      iconFamily='material-icons'
+                      iconName='account_circle'
+                      error={
+                        errors.nameOnCard &&
+                        touched.nameOnCard &&
+                        errors.nameOnCard
+                      }
+                      success={
+                        !errors.nameOnCard && touched.nameOnCard ? 'Valid' : ''
+                      }
+                    />
+                  </div>
 
-                <div className='pb-5 pt-2'>
-                  <InputIcon
-                    name='cardNumber'
-                    defaultValue={prevvalues.cardNumber}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    type='text'
-                    color='indigo'
-                    size='regular'
-                    outline={true}
-                    placeholder='Card Number'
-                    iconFamily='material-icons'
-                    iconName='credit_card'
-                    error={
-                      errors.cardNumber &&
-                      touched.cardNumber &&
-                      errors.cardNumber
-                    }
-                    success={
-                      !errors.cardNumber && touched.cardNumber ? 'Valid' : ''
-                    }
-                  />
-                </div>
-                <div className='pb-5 pt-2'>
-                  <InputIcon
-                    name='cvv'
-                    defaultValue={prevvalues.cvv}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    type='number'
-                    color='indigo'
-                    size='regular'
-                    outline={true}
-                    placeholder='Cvv#'
-                    iconFamily='material-icons'
-                    iconName='subtitles'
-                    error={errors.cvv && touched.cvv && errors.cvv}
-                    success={!errors.cvv && touched.cvv ? 'Valid' : ''}
-                  />
-                </div>
+                  <div className='pb-5 pt-2'>
+                    <InputIcon
+                      name='cardNumber'
+                      defaultValue={prevvalues.cardNumber}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      type='text'
+                      color='indigo'
+                      size='regular'
+                      outline={true}
+                      placeholder='Card Number'
+                      iconFamily='material-icons'
+                      iconName='credit_card'
+                      error={
+                        errors.cardNumber &&
+                        touched.cardNumber &&
+                        errors.cardNumber
+                      }
+                      success={
+                        !errors.cardNumber && touched.cardNumber ? 'Valid' : ''
+                      }
+                    />
+                  </div>
+                  <div className='pb-5 pt-2'>
+                    <InputIcon
+                      name='cvv'
+                      defaultValue={prevvalues.cvv}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      type='number'
+                      color='indigo'
+                      size='regular'
+                      outline={true}
+                      placeholder='Cvv#'
+                      iconFamily='material-icons'
+                      iconName='subtitles'
+                      error={errors.cvv && touched.cvv && errors.cvv}
+                      success={!errors.cvv && touched.cvv ? 'Valid' : ''}
+                    />
+                  </div>
 
-                <div className='flex justify-center pt-2'>
-                  <Button
-                    type='submit'
-                    size='lg'
-                    onSubmit={handleSubmit}
-                    color='indigo'
-                    ripple='dark'
-                    disabled={isSubmitting}
-                  >
-                    Next Step
-                  </Button>
-                </div>
-              </form>
-            </CardBody>
-          </Card>
+                  <div className='flex justify-center pt-2'>
+                    <Button
+                      type='submit'
+                      size='lg'
+                      onSubmit={handleSubmit}
+                      color='indigo'
+                      ripple='dark'
+                      disabled={isSubmitting}
+                    >
+                      Next Step
+                    </Button>
+                  </div>
+                </form>
+              </CardBody>
+            </Card>
+          </div>
         )}
       </Formik>
     </div>
